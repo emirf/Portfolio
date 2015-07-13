d9b55c82da735f3897a0b54821435dde5d8528fc
Emir Fils-Aime <ejsfils@gmail.com>
2015-07-13 18:32:37 -0400

    Initial Commit: Created a landing page for portfolio and an index page for all class work

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+var passThis = "CodeMe";
+var pass;
+
+function enterThis(event){
+	if(event.which == 13 || event.which == 13) {
+		validateThis();
+		return false;
+	}
+	return true;
+}
+
+function validateThis() {
+	pass = document.getElementById("pWord").value;
+	if (pass === passThis) {
+		window.location="index.html";
+	}
+}
